132241a8d853aa5f8c9aae13eb946ff78546a8d3
tinaxgao <89364961+tinaxgao@users.noreply.github.com>
2021-10-15 09:52:56 -0400

    started to set up css, header & nav

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React, { useState, useEffect } from "react";
 import { Link, Route, Switch } from "react-router-dom";
+import "./App.css";
 
 const App = () => {
   return (
-    <>
-      <Link to="/">
-        <h1>Lambda Eats</h1>
-      </Link>
-      <div className="header">
+    <div className="App">
+      <header>
+        <Link to="/">
+          <h1>Lambda Eats</h1>
+        </Link>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/">Help</Link>
+        </nav>
+      </header>
+      <div className="hero">
         <p>Your favorite food delivered while you code!</p>
         <Link to="INSERTORDERPAGE">Order</Link>
       </div>
-    </>
+      <Switch>
+        
+      </Switch>
+    </div> //ends app wrapper
   );
 };
 export default App;
